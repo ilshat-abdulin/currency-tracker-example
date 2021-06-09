448f8493d6d6cd255014a8b4a2e280b868cc7cdd
abdulin.il <abdulin.ilshat@gmail.com>
2021-06-09 21:02:02 +0500

    database created, refactor

--- app/src/main/java/com/android/getcrypto/database/AppDatabase.kt
+++ app/src/main/java/com/android/getcrypto/database/AppDatabase.kt
@@ -1,27 +1,32 @@
 package com.android.getcrypto.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.android.getcrypto.pojo.CoinPriceInfo
 
-abstract class Database : RoomDatabase() {
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
 
     companion object{
-        private var db: Database? = null
+        private var db: AppDatabase? = null
         private const val DB_NAME = "main.db"
         private val LOCK = Any()
 
-        fun getInstance(context: Context): Database{
+        fun getInstance(context: Context): AppDatabase{
             synchronized(LOCK) {
                 db?.let { return it }
                 val instance = Room.databaseBuilder(
                         context,
-                        Database::class.java,
+                        AppDatabase::class.java,
                         DB_NAME).build()
                 db = instance
                 return instance
             }
             }
         }
+    abstract fun coinPriceInfoDao(): CoinPriceInfoDao
+
     }
 
